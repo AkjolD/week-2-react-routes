f91afaf38f1939d1ded200d6a6426682ee6671dd
AkjolD <akjol.dunganov1@gmil.com>
2020-11-12 03:58:23 +0600

    fixed header repository name

--- client/components/header.js
+++ client/components/header.js
@@ -2,15 +2,17 @@ import React from 'react'
 import {Link, useParams} from 'react-router-dom'
 
 const Header = () => {
-  const {userName} = useParams()
+  const {userName, repositoryName } = useParams()
   return (
     <nav className="flex items-center text-white justify-between flex-wrap bg-teal-500 p-6">
       <div id='repository-name' className="flex items-center flex-shrink-0 mr-6">
         {userName}
       </div>
-      <Link id='go-repository-list' to={`/${userName}`}>
-        Go repository list
-      </Link>
+      {repositoryName && (
+        <Link id='go-repository-list' to={`/${userName}`}>
+          Go repository list
+        </Link>
+      )}
       <Link id='go-back' to='/'>
         Go back
       </Link>
